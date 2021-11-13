0e907f0195d24101a8b096055efca1918ec1b362
Luís Clício <cliciosa14@gmail.com>
2021-11-13 09:37:57 -0300

    feat: uses factory client

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print('Hello world')
+import client
+from factories.car_factories import CarFactoryA, CarFactoryB
 
+
+if __name__ == '__main__':
+    client.factory_client(CarFactoryA())
+    client.factory_client(CarFactoryB())
